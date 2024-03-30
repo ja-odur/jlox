369b881cf4930d51dd78738ef5922166ce69090f
ja-odur <jodur@mfsafrica.com>
2024-03-30 21:45:59 +0300

    Fix AstPrinter

--- java/com/craftinginterpreters/lox/AstPrinter.java
+++ java/com/craftinginterpreters/lox/AstPrinter.java
@@ -11,6 +11,11 @@ public class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitCallExpr(Expr.Call expr) {
+        return parenthesize("call", expr.callee);
+    }
+
+    @Override
     public String visitLogicalExpr(Expr.Logical expr) {
         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
     }
