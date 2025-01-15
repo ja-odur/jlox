3ff307d1b75746d7caf7e19aa254e645098b0c23
ja-odur <odurjoseph8@gmail.com>
2025-01-15 12:16:13 +0300

    Add garbage collector debug logging

--- c/memory.h
+++ c/memory.h
@@ -16,6 +16,7 @@
 #define FREE_ARRAY(type, pointer, oldCount) reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void collectGarbage();
 void freeObjects();
 
 #endif
