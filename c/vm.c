f9b66c1d475f911e376236eaa26293592b6d952a
ja-odur <odurjoseph8@gmail.com>
2025-01-28 09:00:58 +0300

    Instance initializer setup.

--- c/vm.c
+++ c/vm.c
@@ -69,6 +69,9 @@ void initVM() {
 
     initTable(&vm.globals);
     initTable(&vm.strings);
+    
+    vm.initString = NULL; // Zero out field to avoid GC interruptions due to string copying below.
+    vm.initString = copyString("init", 4);
 
     defineNative("clock", clockNative);
 }
@@ -76,6 +79,7 @@ void initVM() {
 void freeVM() {
     freeTable(&vm.globals);
     freeTable(&vm.strings);
+    vm.initString = NULL;
     freeObjects();
 }
 
@@ -122,6 +126,15 @@ static bool callValue(Value callee, int argCount) {
             case OBJ_CLASS: {
                 ObjClass* klass = AS_CLASS(callee);
                 vm.stackTop[-argCount - 1] = OBJ_VAL(newInstance(klass));
+                Value initializer;
+                
+                if (tableGet(&klass->methods, vm.initString, &initializer)) {
+                    return call(AS_CLOSURE(initializer), argCount);
+                } else if (argCount != 0) {
+                    runtimeError("Expected 0 arguments but got %d.", argCount);
+                    return false;
+                }
+                
                 return true;
             }
             case OBJ_CLOSURE:
